8c05149eb4ea2a6e80376c2ec21bc4a92037d5dd
Prof Manjaro <ovidiojf@gmail.com>
2024-11-28 23:28:19 -0300

    Add tests for priority queue

--- priority_queue/priority_queue.py
+++ priority_queue/priority_queue.py
@@ -1,15 +1,31 @@
+import heapq
+
 
 class PriorityQueue:
-    heap = []
 
-    def push(element, priority):
-        pass
+    def __init__(self):
+        self.heap = []
 
-    def pop():
-        pass
 
-    def peek():
-        pass
+    def push(self, priority, element):
+        if not isinstance(priority, (int, float)):
+            raise TypeError("Priority must be a number")
+        if element is None:
+            raise ValueError("Element can't be None")
+        heapq.heappush(self.heap, (priority, element))
+
+
+    def pop(self):
+        if not self.heap:
+            raise IndexError("Can't pop from an empty queue")
+        return heapq.heappop(self.heap)[1]
+
+
+    def peek(self):
+        if not self.heap:
+            return None
+
+        return self.heap[0][1]
 
 
 
--- priority_queue/test_priority_queue.py
+++ priority_queue/test_priority_queue.py
@@ -1,22 +1,81 @@
-import heapq
+import unittest
+from priority_queue import PriorityQueue
 
 
-def main():
-    print("--- Test built in Priority Queue ---\n")
+class CustomTextTestResult(unittest.TextTestResult):
 
-    pq = []
+    def startTestRun(self):
+        # Called before any tests start
+        super().startTestRun()
+        print("Commencing the watch")
 
-    heapq.heappush(pq, (3, "Task 3"))
-    heapq.heappush(pq, (1, "Task 1"))
-    heapq.heappush(pq, (2, "Task 2"))
 
-    while pq:
-        priority, task = heapq.heappop(pq)
-        print(f'Priority: {priority}, task: {task}')
+    def printErrors(self):
+        super().printErrors()
+        if not self.failures and not self.errors:
+            print("All secure on this quiet evening")
 
+
+class CustomTestRunner(unittest.TextTestRunner):
+    def _makeResult(self):
+        return CustomTextTestResult(self.stream,
+                                    self.descriptions,
+                                    self.verbosity)
+
+
+class TestPriorityQueue(unittest.TestCase):
+
+    def setUp(self):   # called before each test
+        self.pq = PriorityQueue()
+
+
+    def test_push_pop(self):
+
+        with self.assertRaises(TypeError):
+            self.pq.push("Im not a number", "Element")
+
+        with self.assertRaises(ValueError):
+            self.pq.push(1, None)
+
+        self.pq.push(2, "Task BBB")
+        self.pq.push(1, "Task AAA")
+        self.pq.push(3, "Task CCC")
+        self.pq.push(-1, "Negative priority")
+        self.pq.push(0.5, "Float priority")
+
+        self.assertEqual(self.pq.pop(), "Negative priority")
+        self.assertEqual(self.pq.pop(), "Float priority")
+        self.assertEqual(self.pq.pop(), "Task AAA")
+        self.assertEqual(self.pq.pop(), "Task BBB")
+        self.assertEqual(self.pq.pop(), "Task CCC")
+
+        with self.assertRaises(IndexError):
+            self.pq.pop()
+
+
+    def test_peek(self):
+        self.assertIsNone(self.pq.peek())
+
+        self.pq.push(2, "Task BBB")
+        self.pq.push(1, "Task AAA")
+        self.pq.push(3, "Task CCC")
+
+        self.assertEqual("Task AAA", self.pq.peek())
+
+
+    def test_same_priorities(self):
+        self.pq.push(1, "Task AAA")
+        self.pq.push(1, "Task BBB")
+        self.pq.push(1, "Task CCC")
+
+        self.assertEqual(self.pq.pop(), "Task AAA")
+        self.assertEqual(self.pq.pop(), "Task BBB")
+        self.assertEqual(self.pq.pop(), "Task CCC")
 
 
 if __name__ == "__main__":
-    main()
+    #  unittest.main(testRunner=CustomTestRunner)
+    unittest.main()
+
 
 
